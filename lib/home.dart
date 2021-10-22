06185df2942f0b0972e985b376f13839a93e4ad0
Phuc-HuynhTrong <phuchuynh020601@gmail.com>
2021-10-22 17:08:48 +0700

    add some poster  create home.dart

---
+++ lib/home.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:unflix/screens/home_page/home_page_sreen.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int index = 0;
+  @override
+  void onTapedTap(int id) {
+    setState(() {
+      index = id;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Container(
+          child: index == 0
+              ? HomePageScreen()
+              : index == 1
+              ? Container()
+              : index == 2
+              ? Container()
+              : Container(),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+                label: "Trang chủ",
+                icon: Icon(
+                  Icons.water_damage_outlined,
+                  size: 30,
+                  //color: Colors.white,
+                )),
+            BottomNavigationBarItem(
+                label: "Sắp ra mắt",
+                icon: Icon(
+                  Icons.upcoming,
+                  size: 30,
+                  // color: Colors.white,
+                )),
+            BottomNavigationBarItem(
+                label: "Tìm kiếm",
+                icon: Icon(
+                  Icons.search,
+                  size: 30,
+                  //color: Colors.white,
+                )),
+            BottomNavigationBarItem(
+                label: "Tệp tải xuống",
+                icon: Icon(
+                  Icons.download,
+                  size: 30,
+                  //color: Colors.white,
+                )),
+          ],
+          currentIndex: index,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey[500],
+          onTap: onTapedTap,
+        ));
+  }
+}
+
