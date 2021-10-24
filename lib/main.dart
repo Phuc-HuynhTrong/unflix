5fc1b79e852800385d39f54569f5495f05d6a334
Lam <lamtk.84@gmail.com>
2021-10-24 09:37:52 +0700

    init theme data

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: const Color(0xffF6C700),
+        backgroundColor: Colors.black,
+        //hintColor: const Color(0xff9C9C9C),
+        hintColor: const Color(0xff595959),
+        indicatorColor: const Color(0xffC4C4C4).withOpacity(0.44),
+        cardColor: const Color(0xff121212),
+        fontFamily: 'Noto Sans'
+      ),
       home: Home(),
     );
   }
