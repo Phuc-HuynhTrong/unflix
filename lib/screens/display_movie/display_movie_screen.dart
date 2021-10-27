c94e036c890515465dacce8d0db09c9acb816db3
Phuc-HuynhTrong <phuchuynh020601@gmail.com>
2021-10-27 23:28:59 +0700

    create all button in screen

--- lib/screens/display_movie/display_movie_screen.dart
+++ lib/screens/display_movie/display_movie_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 
 class DisplayMovieSceen extends StatefulWidget {
@@ -12,10 +13,13 @@ class _DisplayMovieSceenState extends State<DisplayMovieSceen> {
   late VideoPlayerController _controller;
   late Future<void> _initializeVideoPlayerFuture;
   bool showAllButton = false;
+  double volumnDefault = 0.75;
+  double speed = 1;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    SystemChrome.setEnabledSystemUIOverlays([]);
     _controller = VideoPlayerController.asset(
         'assets/videos/THE-QUEEN_S-GAMBIT-Trailer-_2020_.mp4');
     _initializeVideoPlayerFuture = _controller.initialize();
@@ -39,6 +43,8 @@ class _DisplayMovieSceenState extends State<DisplayMovieSceen> {
           children: [
             MaterialButton(
               padding: EdgeInsets.all(0),
+              highlightColor: Colors.transparent,
+              splashColor: Colors.transparent,
               onPressed: () {
                 setState(() {
                   if (showAllButton == true) {
@@ -64,17 +70,22 @@ class _DisplayMovieSceenState extends State<DisplayMovieSceen> {
                           children: [
                             VideoPlayer(_controller),
                             Visibility(
-                              visible:  _controller.value.isPlaying
+                              visible: _controller.value.isPlaying
                                   ? showAllButton
-                                  ? false
-                                  : true
+                                      ? false
+                                      : true
                                   : true,
                               child: Container(
                                 height: 10,
-                                margin: EdgeInsets.only(top:MediaQuery.of(context).size.height*5/6,),
+                                margin: EdgeInsets.only(
+                                  top: MediaQuery.of(context).size.height *
+                                      5 /
+                                      6,
+                                ),
                                 width: MediaQuery.of(context).size.width,
-                                child: VideoProgressIndicator(_controller,
-                                    allowScrubbing: true,
+                                child: VideoProgressIndicator(
+                                  _controller,
+                                  allowScrubbing: true,
                                 ),
                               ),
                             ),
@@ -92,49 +103,217 @@ class _DisplayMovieSceenState extends State<DisplayMovieSceen> {
                 ),
               ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Visibility(
-                      visible: _controller.value.isPlaying
-                          ? showAllButton
-                              ? false
-                              : true
-                          : true,
-                      child: TextButton(
-                        onPressed: () {
-                          setState(() {
-                            if (_controller.value.isPlaying) {
-                              _controller.pause();
-                            } else {
-                              _controller.play();
-                            }
-                          });
-                        },
-                        child: Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              border:
-                                  Border.all(color: Colors.white, width: 1)),
-                          child: Icon(
-                            _controller.value.isPlaying
-                                ? Icons.pause
-                                : Icons.play_arrow,
-                            color: Colors.white,
-                            size: 40,
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Visibility(
+                    visible: showAllButton ? false : true,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(
+                          Icons.cast,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        Center(
+                          child: Text(
+                            '"GAMBIT HẬU"',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w800,
+                            ),
                           ),
                         ),
-                      ),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              'Exit',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 20),
+                            ))
+                      ],
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  Visibility(
+                    visible: showAllButton ? false : true,
+                    child: Row(
+                      children: [
+                        RotationTransition(
+                            turns: new AlwaysStoppedAnimation(270 / 360),
+                            child: Container(
+                                height: 20,
+                                child:
+                                    Slider(value: 0.5, onChanged: (val) {}))),
+                        Container(
+                          margin: EdgeInsets.only(right: 20),
+                          child: MaterialButton(
+                            highlightColor: Colors.transparent,
+                            splashColor: Colors.transparent,
+                            onPressed: () {},
+                            child: Container(
+                                height: 40,
+                                width: 40,
+                                child: Image.asset(
+                                  'assets/icons/rotate-left.png',
+                                  fit: BoxFit.fill,
+                                )),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              if (_controller.value.isPlaying) {
+                                _controller.pause();
+                              } else {
+                                _controller.play();
+                                showAllButton = true;
+                              }
+                            });
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                border:
+                                    Border.all(color: Colors.white, width: 1)),
+                            child: Icon(
+                              _controller.value.isPlaying
+                                  ? Icons.pause
+                                  : Icons.play_arrow,
+                              color: Colors.white,
+                              size: 40,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 20),
+                          child: MaterialButton(
+                            padding: EdgeInsets.all(0),
+                            highlightColor: Colors.transparent,
+                            splashColor: Colors.transparent,
+                            onPressed: () {},
+                            child: Container(
+                                height: 40,
+                                width: 40,
+                                child: Image.asset(
+                                  'assets/icons/rotate-right.png',
+                                  fit: BoxFit.fill,
+                                )),
+                          ),
+                        ),
+                        RotationTransition(
+                            turns: new AlwaysStoppedAnimation(270 / 360),
+                            child: Container(
+                                height: 20,
+                                child:
+                                    Slider(value: 0.5, onChanged: (val) {}))),
+                      ],
+                    ),
+                  ),
+                  Visibility(
+                    visible: showAllButton ? false : true,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        MaterialButton(
+                          padding: EdgeInsets.all(0),
+                          highlightColor: Colors.transparent,
+                          splashColor: Colors.transparent,
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.asset('assets/icons/bolt.png'),
+                              Text(
+                                'Tốc độ ${speed}x',
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                        MaterialButton(
+                          padding: EdgeInsets.all(0),
+                          highlightColor: Colors.transparent,
+                          splashColor: Colors.transparent,
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.asset('assets/icons/LockOpen.png'),
+                              Text(
+                                'Khóa',
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                        MaterialButton(
+                          padding: EdgeInsets.all(0),
+                          highlightColor: Colors.transparent,
+                          splashColor: Colors.transparent,
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.asset('assets/icons/Stack.png'),
+                              Text(
+                                'Các tập',
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                        MaterialButton(
+                          padding: EdgeInsets.all(0),
+                          highlightColor: Colors.transparent,
+                          splashColor: Colors.transparent,
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.asset('assets/icons/ChatTeardropDots.png'),
+                              Text(
+                                'Âm thanh và phụ đề',
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                        MaterialButton(
+                          padding: EdgeInsets.all(0),
+                          highlightColor: Colors.transparent,
+                          splashColor: Colors.transparent,
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.asset('assets/icons/SkipForward.png'),
+                              Text(
+                                'Tập tiếp theo',
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
